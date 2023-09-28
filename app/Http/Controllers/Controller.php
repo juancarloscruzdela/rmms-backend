9a65b1ef1956c4e54fcb7a3ae7b60eb284d0793f
juan.carlos <juan.carlos@rakwireless.com>
2023-09-28 11:54:00 +0800

    mod: update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,13 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function getIp(){
-        $myIP = gethostbyname(trim(`hostname`));
-        echo $myIP;
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        echo $ip;
     }
 }
